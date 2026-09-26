760bb399870ccaf8197f252d788288ef21d047e1
Kawaki227 <zoro227.ne@gmail.com>
2026-09-26 02:12:29 +0100

    Delete public/sw.js

--- public/sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='chop-city-v7';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest']))));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&e.request.url.startsWith(self.location.origin))e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
